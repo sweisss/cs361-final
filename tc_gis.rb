7d733528946b47580e390515cb5fde7442729af3
Seth Weiss <sweisss@gmail.com>
2022-12-05 14:43:01 -0800

    Changed w to wp to differentiate Waypoint from World.

--- tc_gis.rb
+++ tc_gis.rb
@@ -6,19 +6,19 @@ require 'test/unit'
 
 class TestGis < Test::Unit::TestCase
   def test_waypoints
-    w = Waypoint.new(-121.5, 45.5, 30, 'home', 'flag')
+    wp = Waypoint.new(-121.5, 45.5, 30, 'home', 'flag')
     expected = JSON.parse('{"type": "Feature","properties": {"title": "home","icon": "flag"},"geometry": {"type": "Point","coordinates": [-121.5,45.5,30]}}')
-    result = JSON.parse(w.get_waypoint_json)
+    result = JSON.parse(wp.get_waypoint_json)
     assert_equal(result, expected)
 
-    w = Waypoint.new(-121.5, 45.5, nil, nil, 'flag')
+    wp = Waypoint.new(-121.5, 45.5, nil, nil, 'flag')
     expected = JSON.parse('{"type": "Feature","properties": {"icon": "flag"},"geometry": {"type": "Point","coordinates": [-121.5,45.5]}}')
-    result = JSON.parse(w.get_waypoint_json)
+    result = JSON.parse(wp.get_waypoint_json)
     assert_equal(result, expected)
 
-    w = Waypoint.new(-121.5, 45.5, nil, 'store', nil)
+    wp = Waypoint.new(-121.5, 45.5, nil, 'store', nil)
     expected = JSON.parse('{"type": "Feature","properties": {"title": "store"},"geometry": {"type": "Point","coordinates": [-121.5,45.5]}}')
-    result = JSON.parse(w.get_waypoint_json)
+    result = JSON.parse(wp.get_waypoint_json)
     assert_equal(result, expected)
   end
 
@@ -48,8 +48,8 @@ class TestGis < Test::Unit::TestCase
   end
 
   def test_world
-    w = Waypoint.new(-121.5, 45.5, 30, 'home', 'flag')
-    w2 = Waypoint.new(-121.5, 45.6, nil, 'store', 'dot')
+    wp = Waypoint.new(-121.5, 45.5, 30, 'home', 'flag')
+    wp2 = Waypoint.new(-121.5, 45.6, nil, 'store', 'dot')
     ts1 = [
       Point.new(-122, 45),
       Point.new(-122, 46),
@@ -66,7 +66,7 @@ class TestGis < Test::Unit::TestCase
     t = Track.new([ts1, ts2], 'track 1')
     t2 = Track.new([ts3], 'track 2')
 
-    w = World.new('My Data', [w, w2, t, t2])
+    w = World.new('My Data', [wp, wp2, t, t2])
 
     expected = JSON.parse('{"type": "FeatureCollection","features": [{"type": "Feature","properties": {"title": "home","icon": "flag"},"geometry": {"type": "Point","coordinates": [-121.5,45.5,30]}},{"type": "Feature","properties": {"title": "store","icon": "dot"},"geometry": {"type": "Point","coordinates": [-121.5,45.6]}},{"type": "Feature", "properties": {"title": "track 1"},"geometry": {"type": "MultiLineString","coordinates": [[[-122,45],[-122,46],[-121,46]],[[-121,45],[-121,46]]]}},{"type": "Feature", "properties": {"title": "track 2"},"geometry": {"type": "MultiLineString","coordinates": [[[-121,45.5],[-122,45.5]]]}}]}')
     result = JSON.parse(w.to_geojson)
